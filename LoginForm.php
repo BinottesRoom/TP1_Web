d80f9e190dc457fe7e94ac3a109f2357c66263c3
jujujuc <55589637+jujujuc@users.noreply.github.com>
2019-10-02 14:00:18 -0400

    Update LoginForm.php

--- LoginForm.php
+++ LoginForm.php
@@ -2,9 +2,9 @@
 require 'Matos pour le TP/utilities/htmlHelper.php';
 $content = "<form method='POST'>"
 
-<input type=\"text\" name=\"User\" placeholder=\"Nom d'usager\"><br>
-<input type=\"password\" name=\"Password\" placeholder=\"Mot de passe\"><br>
-<input type=\"submit\" name=\"login\" value=\"Soumettre\"><br>
+"<input type='text' name='User' placeholder=\"Nom d'usager\"><br>
+<input type='password' name='Password' placeholder='Mot de passe'><br>
+<input type='submit' name='login' value='Soumettre'><br>
 ";
 $content .= html_close("form");
 require_once 'MasterPage.php';
