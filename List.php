5dd91c7d701970cf0b7f6f851663d3543864f669
BigBinotte <55589663+BigBinotte@users.noreply.github.com>
2019-10-02 14:03:06 -0400

    yeet

--- List.php
+++ List.php
@@ -1,5 +1,31 @@
 <?php
+
+$content = "<table>
+<tr>
+        <th>
+            Titre
+        </th>
+        <th>
+            Description
+        </th>
+        <th>
+            URL
+        </th>
+        <th>
+    
+        </th>
+        <th>
+            Source
+        </th>
+        <th>
+            Image
+        </th>
+        <th>
+    
+        </th>
+</tr>
+
+</table>";
+
 include_once "MasterPage.php";
-
-
 ?>
