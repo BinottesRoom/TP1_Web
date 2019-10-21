94b45990d1115b06da7390f8b5e34927c467b7ac
jujujuc <55589637+jujujuc@users.noreply.github.com>
2019-10-21 13:19:05 -0400

    a

--- AllJsLinks.php
+++ AllJsLinks.php
@@ -2,5 +2,6 @@
 echo "<script src=\"Matos pour le TP/js/jquery-3.3.1.js\"></script>";
 echo "<script src=\"Matos pour le TP/js/jquery.maskedinput.js\"></script>";
 echo "<script src=\"Matos pour le TP/js/Validation.js\"></script>";
-echo "<script src=\"Matos pour le TP/js/bookmarkFormValidation.js";
+echo "<script src=\"Matos pour le TP/js/bookmarkFormValidation.js\"></script>;
+
 ?>
