35a6327a83399832643a94f5c94b69e1e3e41492
BigBinotte <55589663+BigBinotte@users.noreply.github.com>
2019-10-23 09:04:10 -0400

    yeet

--- List.php
+++ List.php
@@ -28,27 +28,50 @@ $content = html_open("div").html_open("b")."Favoris".html_close("b").html_close(
         .html_close("div")
     .html_close("hr");
 
-foreach(readBookmarks() as $key => $ligne)
+
+$bookmarks = readBookmarks();
+foreach($bookmarks as $ligne)
 {
     $content .= "<div class='bookmarks-row-layout' >";
     $_SESSION['idFavoris']++;
+    
     foreach($ligne as $key2 => $value)
     {
+        
         if ($ligne["Id"] != $value)
         {
+            if($key2 == 2)
+            {
+                $content .= html_open("div");
+                $content .= html_faviconLinkFromUrl($bookmark['Url']);
+                $content .= html_close("div");
+            }
             $content .= html_open("div");
             $content .= $value;
             $content .= html_close("div");
         }
+
+        
         
     }
-    unset($value);
-    $content .= html_open("div")."<a href='EditForm.php'><img value='".$_SESSION['idFavoris']."' src='images/Modify.png' alt='Ajouter' onclick =".getID(this).">".html_close("a").html_close("div");
-    $content .= html_open("div")."<a href='DeleteForm.php'><img value='".$_SESSION['idFavoris']."' src='images/Erase.png' alt='Effacer' onclick =".getID(this).">".html_close("a").html_close("div");
+    // $shortUrl = str_replace('http://', '', $ligne['Url']);
+    // $shortUrl = str_replace('https://', '', $shortUrl);
+    // $content .= '<div class="bookmark-cell">'.$ligne['Title'].'</div>';
+    // $content .= '<div class="bookmark-cell">'.$ligne['Description'].'</div>';
+    // $content .= '<div class="favicon-cell">'.html_faviconLinkFromUrl($ligne['Url']).html_link($ligne['Url'], $shortUrl, '_blank').'</div>';
+
+    // $content .= '<div class="icon-cell">'.html_icon('images/Modify.png', 'editForm.php?id='.$ligne['Id'], 'Modifier '.$ligne['Title'], 'left').'</div>';
+    // $content .= '<div class="icon-cell">'.html_icon('images/Erase.png', 'deleteForm.php?id='.$ligne['Id'], 'Effacer '.$ligne['Title'], 'left').'</div>';
+
+    // $content .= html_open("div")."<a href='EditForm.php'><img value='".$_SESSION['idFavoris']."' src='images/Modify.png' alt='Ajouter' onclick =".getID(this).">".html_close("a").html_close("div");
+    // $content .= html_open("div")."<a href='DeleteForm.php'><img value='".$_SESSION['idFavoris']."' src='images/Erase.png' alt='Effacer' onclick =".getID(this).">".html_close("a").html_close("div");
+    $content .= html_open("div")."<a href='EditForm.php'><img src='images/Modify.png' alt='Ajouter'>".html_close("a").html_close("div");
+    $content .= html_open("div")."<a href='DeleteForm.php'><img src='images/Erase.png' alt='Effacer'>".html_close("a").html_close("div");
     $content .= html_close("div");
 
 
     $content .= "<form method='post' action='DeleteForm.php'></form>";
+    unset($value);
 }
 
 function getId($idImage)
